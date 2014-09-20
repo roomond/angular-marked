a6e27c1c16362b1c0c1ff426a14cd671b3d21551
J. Harshbarger <hypercubed@gmail.com>
2014-09-20 16:22:33 +0900

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,55 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "AngularJS Markdown using marked.",
+      "keywords": "$scope angularjs api class cols convert default defaults directive example expose function globally hc html js maincontroller markdown marked module my_markdown net ng-controller ng-model option options output overview rows service set span8 time wanted"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.directive:marked",
+      "shortName": "marked",
+      "type": "directive",
+      "moduleName": "hc.marked",
+      "shortDescription": "Compiles source test into HTML.",
+      "keywords": "$scope $templatecache ae angular api bind blank block class cols compiled compiles content defaults directive examplea exampleb examplec fakeinclude file function hash hc html include js maincontroller markdown marked md module my_markdown myapp net ng-controller ng-include ng-model options opts output override rows scope simple source span8 test text var variable view works"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.service:marked",
+      "shortName": "marked",
+      "type": "service",
+      "moduleName": "hc.marked",
+      "shortDescription": "A reference to the marked parser.  Same as $window.marked.",
+      "keywords": "$scope $window api function hc html js maincontroller marked module ng-controller parser reference service test"
+    },
+    {
+      "section": "api",
+      "id": "hc.marked.service:markedProvider",
+      "shortName": "markedProvider",
+      "type": "service",
+      "moduleName": "hc.marked",
+      "shortDescription": "Use markedProvider to change the default behavior of the marked service.",
+      "keywords": "angular api behavior change config default example function gfm google google-code-prettify hc highlight highlighter html include javascript js marked markedprovider method module myapp options options-1 opts org prettyprintone return script service setoptions syntax tables true works"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "marked.js",
+    "angular-marked.js",
+    "script.js"
+  ]
+};
